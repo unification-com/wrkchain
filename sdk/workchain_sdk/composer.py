18866085025d91055a714cdac8b85c4d623f0f1c
Indika Piyasena <indika.piyasena@gmail.com>
2019-03-07 11:03:54 +0700

    sdk.composer: flesh out
    somewhat based on generate_custom_compose.py

--- sdk/workchain_sdk/composer.py
+++ sdk/workchain_sdk/composer.py
@@ -3,6 +3,30 @@ import yaml
 COMPOSE_VERSION = '3.2'
 
 
+def bootnode():
+    name = 'workchain_bootnode'
+    return {
+        name: {
+            'image': 'bootnode',
+            'hostname': name,
+            'container_name': name,
+            'ports': ['30301:30301', '30303:30303']
+        }
+    }
+
+
+def generate_validators(n: int):
+    d = {}
+    for i in range(n):
+        name = f'workchain-validator-{i+1}'
+        d[name] = {
+                'image': 'validator',
+                'hostname': name,
+                'container_name': name,
+            }
+    return d
+
+
 def generate():
     # preserve dict ordering when dumping
     yaml.add_representer(
@@ -13,5 +37,10 @@ def generate():
     compose_yaml = dict(
         version=COMPOSE_VERSION
     )
+
+    evs = generate_validators(3)
+    compose_yaml['services'] = dict(
+        list(bootnode().items()) + list(evs.items()))
+
     composition = yaml.dump(compose_yaml)
     return composition
