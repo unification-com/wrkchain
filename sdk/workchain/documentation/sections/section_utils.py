3e3979bf209aab3a7ea71f10b1b0d8072adabe7d
Codegnosis <paul@codegnosis.co.uk>
2019-03-12 11:11:31 +0000

    sdk.documentation - fix missing bootnode IP/Port bug

--- sdk/workchain/documentation/sections/section_utils.py
+++ sdk/workchain/documentation/sections/section_utils.py
@@ -3,11 +3,11 @@ class SectionUtils:
     @staticmethod
     def set_geth_bootnode_flag(bootnode_address, bootnode_ip,
                                bootnode_port):
-        bootnode_enode = f'{bootnode_address}@' \
+        bootnode_enode = f'enode://{bootnode_address}@' \
             f'{bootnode_ip}:' \
             f'{bootnode_port}'
 
-        bootnode_flag = f'--bootnodes "enode://' \
+        bootnode_flag = f'--bootnodes "' \
             f'{bootnode_enode}" '
 
         return bootnode_enode, bootnode_flag
