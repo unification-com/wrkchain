5d18e101493b07ac0250145fbbdc480ee6cce5a2
Indika Piyasena <indika.piyasena@gmail.com>
2019-03-12 16:40:51 +0700

    sdk.setup: add requirements

--- sdk/setup.py
+++ sdk/setup.py
@@ -4,6 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+requires = [
+    'click>=6.7',
+    'docker-compose>=1.24.0rc1',
+    'pypandoc>=1.4',
+    'PyYAML>=4.2b1',
+    'web3>=4.8.2'
+]
+
 setuptools.setup(
     name="workchain",
     version="0.0.2",
@@ -14,6 +22,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/unification-com/workchain",
     packages=setuptools.find_packages(),
+    install_requires=requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
