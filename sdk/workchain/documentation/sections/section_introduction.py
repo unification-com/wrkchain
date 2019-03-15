a0e4f4304af17dd9dc5530e77b1e23553f12316e
Codegnosis <paul@codegnosis.co.uk>
2019-03-15 15:07:19 +0000

    sdk.documentation - flesh out introduction section

--- sdk/workchain/documentation/sections/section_introduction.py
+++ sdk/workchain/documentation/sections/section_introduction.py
@@ -2,12 +2,16 @@ from workchain.documentation.sections.doc_section import DocSection
 
 
 class SectionIntroduction(DocSection):
-    def __init__(self, section_number, title):
+    def __init__(self, section_number, title, workchain_id):
         path_to_md = 'sections/introduction.md'
         DocSection.__init__(self, path_to_md, section_number, title)
 
+        self.__workchain_id = workchain_id
+
     def generate(self):
-        d = {}
+        d = {
+            '__WORKCHAIN_NETWORK_ID__': self.__workchain_id
+        }
         self.add_content(d, append=False)
         return self.get_contents()
 
@@ -16,8 +20,9 @@ class SectionIntroductionBuilder:
     def __init__(self):
         self.__instance = None
 
-    def __call__(self, section_number, title, oracle_addresses, **_ignored):
+    def __call__(self, section_number, title, workchain_id, **_ignored):
 
         if not self.__instance:
-            self.__instance = SectionIntroduction(section_number, title)
+            self.__instance = SectionIntroduction(section_number, title,
+                                                  workchain_id)
         return self.__instance
--- templates/docs/md/sections/introduction.md
+++ templates/docs/md/sections/introduction.md
@@ -1,2 +1,6 @@
 
-Some introduction placeholder text
+### $__SECTION_NUMBER__.1 About your Workchain
+
+**Your Workchain Network ID:** $__WORKCHAIN_NETWORK_ID__
+
+### $__SECTION_NUMBER__.1.1 Generated Files
