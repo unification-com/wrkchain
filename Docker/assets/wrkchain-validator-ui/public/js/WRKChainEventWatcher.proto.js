94e9a0a1aa0c4c81aa1501e5b0ffd0fa84f2a8d4
Codegnosis <paul@codegnosis.co.uk>
2019-03-18 10:07:23 +0000

    wrkchain rename

--- Docker/assets/wrkchain-validator-ui/public/js/WRKChainEventWatcher.proto.js
+++ Docker/assets/wrkchain-validator-ui/public/js/WRKChainEventWatcher.proto.js
@@ -1,4 +1,4 @@
-function WorkchainEventWatcher(_contractAddress, _web3ProviderUrl, _abi) {
+function WRKChainEventWatcher(_contractAddress, _web3ProviderUrl, _abi) {
 
     let abi = _abi.replace(/\\(.)/mg, "$1");
 
@@ -11,16 +11,16 @@ function WorkchainEventWatcher(_contractAddress, _web3ProviderUrl, _abi) {
 
     this.web3js = new Web3(new Web3.providers.HttpProvider(_web3ProviderUrl));
 
-    this.workchainRootContract = new this.web3js.eth.Contract(this.abi, this.contractAddress);
+    this.wrkchainRootContract = new this.web3js.eth.Contract(this.abi, this.contractAddress);
 
 }
 
-WorkchainEventWatcher.prototype.getLatestRecordedHeader = function(_callback) {
+WRKChainEventWatcher.prototype.getLatestRecordedHeader = function(_callback) {
 
     let self = this;
 
     this.getCurrentBlockNumber().then(blockNumber => {
-        self.workchainRootContract.getPastEvents('RecordHeader', {
+        self.wrkchainRootContract.getPastEvents('RecordHeader', {
             fromBlock: blockNumber -1,
             toBlock: 'latest'
         }, (error, events) => {
@@ -41,7 +41,7 @@ WorkchainEventWatcher.prototype.getLatestRecordedHeader = function(_callback) {
     });
 }
 
-WorkchainEventWatcher.prototype.getCurrentBlockNumber = async function () {
+WRKChainEventWatcher.prototype.getCurrentBlockNumber = async function () {
     let blockNumber = await this.web3js.eth.getBlockNumber();
     console.log("blockNumber:",blockNumber);
     return blockNumber;
