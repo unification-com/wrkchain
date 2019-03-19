77c16108cd9b9230c4192d35ea277fc3abbaef6e
Codegnosis <paul@codegnosis.co.uk>
2019-03-19 10:55:45 +0000

    sdk.documentation - correctly pass through section number to bootnode installation

--- sdk/wrkchain/documentation/sections/section_installation.py
+++ sdk/wrkchain/documentation/sections/section_installation.py
@@ -1,3 +1,5 @@
+from string import Template
+
 from wrkchain.documentation.sections.doc_section import DocSection
 
 
@@ -7,17 +9,21 @@ class SectionInstallationGeth(DocSection):
         DocSection.__init__(self, path_to_md, section_number, title)
 
         self.__bootnode_config = bootnode_config
+        self.__section_number = section_number
 
     def generate(self):
-        install_bootnode = ""
+        install_bootnode_content = ""
         if self.__bootnode_config['type'] == 'dedicated':
             install_bootnode_md = f'templates/docs/md/sections/' \
                 f'install_geth_bootnode.md'
             install_bootnode_md_path = self.root_dir / install_bootnode_md
             install_bootnode = install_bootnode_md_path.read_text()
+            t = Template(install_bootnode)
+            install_bootnode_content = \
+                t.substitute({'__SECTION_NUMBER__': self.__section_number})
 
         d = {
-            '__INSTALL_GETH_BOOTNODE__': install_bootnode
+            '__INSTALL_GETH_BOOTNODE__': install_bootnode_content
         }
         self.add_content(d, append=False)
         return self.get_contents()
