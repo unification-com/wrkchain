b286bdd4c8f9430d12c403edd168633a24cdaa77
Codegnosis <paul@codegnosis.co.uk>
2019-03-19 13:06:15 +0000

    sdk.documentation - function for install bootnode section

--- sdk/wrkchain/documentation/sections/section_installation.py
+++ sdk/wrkchain/documentation/sections/section_installation.py
@@ -14,19 +14,21 @@ class SectionInstallationGeth(DocSection):
     def generate(self):
         install_bootnode_content = ""
         if self.__bootnode_config['type'] == 'dedicated':
-            install_bootnode_md = f'templates/docs/md/sections/' \
-                f'install_geth_bootnode.md'
-            install_bootnode_md_path = self.root_dir / install_bootnode_md
-            install_bootnode = install_bootnode_md_path.read_text()
-            t = Template(install_bootnode)
-            install_bootnode_content = \
-                t.substitute({'__SECTION_NUMBER__': self.__section_number})
+            install_bootnode_content = self.__install_bootnode()
 
         d = {
             '__INSTALL_GETH_BOOTNODE__': install_bootnode_content
         }
         self.add_content(d, append=False)
         return self.get_contents()
+
+    def __install_bootnode(self):
+        install_bootnode_md = f'templates/docs/md/sections/' \
+            f'install_geth_bootnode.md'
+        install_bootnode_md_path = self.root_dir / install_bootnode_md
+        install_bootnode = install_bootnode_md_path.read_text()
+        t = Template(install_bootnode)
+        return t.substitute({'__SECTION_NUMBER__': self.__section_number})
 
 
 class SectionInstallationBuilder:
