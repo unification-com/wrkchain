27ce01b3885030e6d6e500bf7bb1b3ca5b2869a3
Indika Piyasena <indika.piyasena@gmail.com>
2019-03-20 15:12:09 +0700

    sdk: simply copying roles will suffice for now

--- sdk/wrkchain/ansible.py
+++ sdk/wrkchain/ansible.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from shutil import rmtree
+from shutil import copytree, rmtree
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -12,13 +12,24 @@ def generate_directories(build_dir: Path):
     if ansible_dir.exists():
         rmtree(str(ansible_dir))
 
-    ansible_dir.mkdir(parents=True)
+    ansible_dir.mkdir()
+    (ansible_dir / 'roles').mkdir()
+
+
+def copy_role(build_dir: Path, template_dir: Path, role):
+    role_path = template_dir / 'ansible' / 'roles' / role
+
+    ansible_dir = build_dir / 'ansible'
+    role_dir = ansible_dir / 'roles' / role
+
+    copytree(str(role_path), str(role_dir))
 
 
 def generate_ansible(build_dir, config):
     build_root = Path(build_dir)
+    templates = template_root()
 
-    loader = FileSystemLoader(str(template_root() / 'ansible'))
+    loader = FileSystemLoader(str(templates / 'ansible'))
     environment = Environment(loader=loader)
     template = environment.get_template('validator.yml')
     context = {
@@ -27,6 +38,7 @@ def generate_ansible(build_dir, config):
     built = template.render(context)
 
     generate_directories(build_root)
+    copy_role(build_root, templates, 'base')
 
     target = build_root / 'ansible' / 'validator.yml'
     target.write_text(built)
