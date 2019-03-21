1ae57052ea5e29e208aeb0834b2d38b2f8edc43a
Codegnosis <paul@codegnosis.co.uk>
2019-03-21 10:19:54 +0000

    sdk.documentation - flesh out "Running your ORacle" section

--- sdk/wrkchain/documentation/sections/section_oracle.py
+++ sdk/wrkchain/documentation/sections/section_oracle.py
@@ -2,15 +2,21 @@ from wrkchain.documentation.sections.doc_section import DocSection
 
 
 class SectionOracle(DocSection):
-    def __init__(self, section_number, title, oracle_addresses):
+    def __init__(self, section_number, title, oracle_addresses,
+                 mainchain_rpc_uri, wrkchain_id, nodes):
         path_to_md = 'sections/oracle.md'
         DocSection.__init__(self, path_to_md, section_number, title)
 
         self.__oracle_addresses = oracle_addresses
+        self.__mainchain_rpc_uri = mainchain_rpc_uri
+        self.__wrkchain_id = wrkchain_id
+        self.__nodes = nodes
 
     def generate(self):
         d = {
-            '__ORACLE_ADDRESSES__': '\n'.join(self.__oracle_addresses)
+            '__ORACLE_ADDRESSES__': '\n'.join(self.__oracle_addresses),
+            '__WRKCHAIN_NETWORK_ID__': self.__wrkchain_id,
+            '__MAINCHAIN_WEB3_PROVIDER_URL__': self.__mainchain_rpc_uri
         }
         self.add_content(d, append=False)
         return self.get_contents()
@@ -20,9 +26,12 @@ class SectionOracleBuilder:
     def __init__(self):
         self.__instance = None
 
-    def __call__(self, section_number, title, oracle_addresses, **_ignored):
+    def __call__(self, section_number, title, oracle_addresses,
+                 mainchain_rpc_uri, wrkchain_id, nodes, **_ignored):
 
         if not self.__instance:
             self.__instance = SectionOracle(section_number, title,
-                                            oracle_addresses)
+                                            oracle_addresses,
+                                            mainchain_rpc_uri, wrkchain_id,
+                                            nodes)
         return self.__instance
--- templates/docs/md/sections/oracle.md
+++ templates/docs/md/sections/oracle.md
@@ -1,5 +1,22 @@
-Ensure the following addresses have UND on the Mainchain:
+Ensure the following addresses have UND on the Mainchain, since they will
+be used to send data to the WRKCHain Root Smart Contract:
 
 ```text
 $__ORACLE_ADDRESSES__
 ```
+
+One each computer responsible for running the WRKCHain Oracle (usually one or 
+more Validator nodes), run:
+
+```bash
+go run $$GOPATH/src/github.com/unification-com/oracle/oracle.go "WRKCHAIN_WEB3_PROVIDER_URL" "$__MAINCHAIN_WEB3_PROVIDER_URL__" "PRIVATE_KEY_FOR_ORACLE_ADDRESS" "WRKCHAIN_ROOT_CONTRACT_ADDRESS" 10 $__WRKCHAIN_NETWORK_ID__
+```
+
+Where:
+
+`WRKCHAIN_WEB3_PROVIDER_URL` = a Web3 provider URL for your WRKChain, for example
+`http://localhost:8545`  
+`PRIVATE_KEY_FOR_ORACLE_ADDRESS` = the private key for the Oracle Address which 
+will write to the WRKChain Root smart contract  
+`WRKCHAIN_ROOT_CONTRACT_ADDRESS` = the address of your deployed WRKCchain Root
+smart contract on Mainchain
