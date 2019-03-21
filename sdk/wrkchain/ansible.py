15479757aebd58da9ee90870be2fca2807eefbf0
Indika Piyasena <indika.piyasena@gmail.com>
2019-03-21 18:09:05 +0700

    sdk.ansible: generate multiple files per template

--- sdk/wrkchain/ansible.py
+++ sdk/wrkchain/ansible.py
@@ -6,6 +6,18 @@ from jinja2 import Environment, FileSystemLoader
 from wrkchain.utils import template_root
 
 
+class Validators:
+    def __init__(self, context):
+        self.context = context
+
+    def write(self, environment, target, relative):
+        template = environment.get_template(str(relative))
+        for index, validator in enumerate(self.context['validators']):
+            base, ext = str(relative).split('.')
+            dest = target / f'{base}-{index + 1}.{ext}'
+            dest.write_text(template.render(validator))
+
+
 def template_map(source: Path, target: Path, maps: dict):
     loader = FileSystemLoader(str(source))
     environment = Environment(loader=loader)
@@ -18,8 +30,11 @@ def template_map(source: Path, target: Path, maps: dict):
             dest.parent.mkdir(parents=True)
 
         if context:
-            template = environment.get_template(str(relative))
-            dest.write_text(template.render(context))
+            if isinstance(context, Validators):
+                context.write(environment, target, relative)
+            else:
+                template = environment.get_template(str(relative))
+                dest.write_text(template.render(context))
         else:
             copy(str(path), str(dest))
 
@@ -28,13 +43,29 @@ def generate_ansible(build_dir, config):
     build_root = Path(build_dir)
     ansible_dir = build_root / 'ansible'
 
-    validator_one = {
+    provisioner = {
         'home': 'vagrant',
-        'validator_name': 'validator-1'
     }
 
+    validators = {
+        'validators':
+            [
+                {
+                    'home': 'vagrant',
+                    'validator_name': 'validator-1'
+                },
+                {
+                    'home': 'vagrant',
+                    'validator_name': 'validator-2'
+                }
+            ]
+    }
+
+    validator_builder = Validators(validators)
+
     d = {
-        'validator.yml': validator_one,
-        'roles/geth/tasks/main.yml': validator_one,
+        'validator.yml': validator_builder,
+        'Vagrantfile': validators,
+        'roles/geth/tasks/main.yml': provisioner,
     }
     template_map(template_root() / 'ansible', ansible_dir, d)
