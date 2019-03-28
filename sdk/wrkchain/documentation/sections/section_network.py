03e794d38e789ce9f46916e9c7864a2676e14522
Codegnosis <paul@codegnosis.co.uk>
2019-03-28 10:48:21 +0000

    sdk.documentation - improve network section

--- sdk/wrkchain/documentation/sections/section_network.py
+++ sdk/wrkchain/documentation/sections/section_network.py
@@ -2,38 +2,106 @@ from wrkchain.documentation.sections.doc_section import DocSection
 
 
 class SectionNetwork(DocSection):
-    def __init__(self, section_number, title, nodes):
+    def __init__(self, section_number, title, nodes, build_dir,
+                 bootnode_config, wrkchain_id):
         path_to_md = 'network.md'
         DocSection.__init__(self, path_to_md, section_number, title)
         self.__nodes = nodes
+        self.__build_dir = build_dir
+        self.__bootnode_config = bootnode_config
+        self.__wrkchain_id = wrkchain_id
+        self.__section_number = section_number
+        self.__sub_section_number = 1
 
     def generate(self):
 
-        web3_urls = ''
+        web3_providers = []
         for node in self.__nodes:
             if node['rpc']:
-                if isinstance(node['rpc'], bool):
-                    rpc_port = '8545'
-                else:
-                    rpc_port = node["rpc"]["port"]
-                web3_urls += f'<http://{node["ip"]}:{rpc_port}>  \n'
+                rpc = {
+                    'ip': node["ip"],
+                    'port': node["rpc"]["port"],
+                    'title': node['title']
+                }
+                web3_providers.append(rpc)
+
+        configured_rpc_nodes = self.__configured_json_rpc_nodes(web3_providers)
 
         d = {
-            '__JSON_RPC_URLS__': web3_urls
+            '__CONFIGURED_JSON_RPC_NODES__': configured_rpc_nodes,
+            '__RUNNING_LOCAL_JSON_RPC_NODE__':
+                self.__running_local_json_rpc_node()
         }
 
         self.add_content(d, append=False)
         return self.get_contents()
+
+    def __configured_json_rpc_nodes(self, web3_providers):
+        contents = ''
+        if len(web3_providers) > 0:
+            md_file = 'sub/network/configured_json_rpc_nodes.md'
+            t = self.load_sub_section_template(md_file)
+            web3_provider_output = ''
+            for web3 in web3_providers:
+                web3_provider_output += f'**{web3["title"]}**  \n' \
+                    f'**Web3 Provider URL:**<http://{web3["ip"]}:' \
+                    f'{web3["port"]}>  \n' \
+                    f'**Web3 Provider Host:** {web3["ip"]}  \n' \
+                    f'**Web3 Provider Port:** {web3["port"]}  \n' \
+                    f'**Network ID:** {self.__wrkchain_id}\n\n'
+
+            d = {
+                '__SECTION_NUMBER__': self.__section_number,
+                '__SUB_SECTION_NUMBER__': self.__sub_section_number,
+                '__WEB3_PROVIDERS__': web3_provider_output
+            }
+            contents = t.substitute(d)
+            self.__sub_section_number += 1
+
+        return contents
+
+    def __running_local_json_rpc_node(self):
+        doc_build_dir = self.__build_dir.replace('../', '')
+        md_file = 'sub/network/local_json_rpc_node.md'
+        t = self.load_sub_section_template(md_file)
+
+        copy_static_files = ''
+
+        if self.__bootnode_config['type'] == 'static':
+            copy_static_files = f'2. `{doc_build_dir}/static-nodes.json` to ' \
+                f'`~/.ethereum/static-nodes.json`  \n'
+
+        geth_cmd = f'$GOPATH/bin/geth --networkid {self.__wrkchain_id} \\\n' \
+            f'--rpc \\\n' \
+            f'--rpcapi "eth,personal,web3" \\\n' \
+            f'--rpcport "8550" \\\n' \
+            f'--syncmode=light \\\n' \
+            f'--verbosity=4'
+
+        d = {
+            '__SECTION_NUMBER__': self.__section_number,
+            '__SUB_SECTION_NUMBER__': self.__sub_section_number,
+            '__BUILD_DIR__': doc_build_dir,
+            '__COPY_STATIC_FILES__': copy_static_files,
+            '__GETH_COMMAND__': geth_cmd,
+            '__WRKCHAIN_NETWORK_ID__': self.__wrkchain_id
+        }
+
+        contents = t.substitute(d)
+        self.__sub_section_number += 1
+        return contents
 
 
 class SectionNetworkBuilder:
     def __init__(self):
         self.__instance = None
 
-    def __call__(self, section_number, title, nodes, **_ignored):
+    def __call__(self, section_number, title, nodes, build_dir,
+                 bootnode_config, wrkchain_id, **_ignored):
 
         if not self.__instance:
             self.__instance = SectionNetwork(section_number, title,
-                                             nodes)
+                                             nodes, build_dir, bootnode_config,
+                                             wrkchain_id)
 
         return self.__instance
---
+++ templates/docs/md/sections/sub/network/local_json_rpc_node.md
@@ -0,0 +1,38 @@
+### $__SECTION_NUMBER__.$__SUB_SECTION_NUMBER__ Running a local WRKChain JSON RPC Node
+
+It is also possible to install and run a local JSON RPC node, to which you can send
+transactions and queries for your WRKChain. This is the preferred method, since
+it is more secure, and is only available to the computer running the node.
+
+**Install geth**
+
+Follow the instructions to install `geth` on the computer you would like to
+ run a local JSON RPC Node.
+
+**Copying the required files**
+
+Copy the following files to the computer
+
+1. `$__BUILD_DIR__/genesis.json` to `~/.ethereum/genesis.json`
+$__COPY_STATIC_FILES__
+
+**Initialise the node**
+
+```bash
+$$GOPATH/bin/geth init ~/.ethereum/genesis.json
+```
+
+**Running the Node**
+
+Finally, run the node using:
+
+```bash
+$__GETH_COMMAND__
+```
+
+You will be able to connect (for example, via MetaMask) using the following parameters:
+
+**Web3 Provider URL:** http://localhost:8550  
+**Web3 Provider Host:** localhost  
+**Web3 Provider Port:** 8550  
+**Network ID:** $__WRKCHAIN_NETWORK_ID__  
