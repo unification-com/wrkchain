ddfac74823b00cb2fb5b2ababf127a3ac976473a
Codegnosis <paul@codegnosis.co.uk>
2019-04-04 12:57:34 +0100

    sdk.constants - add .und_wrkchain as default datadir

--- sdk/wrkchain/constants.py
+++ sdk/wrkchain/constants.py
@@ -3,6 +3,7 @@ TESTNET_FAUCET_URL = 'http://52.14.173.249/sendtx?to='
 MAINNET_UND_FUND_URL = 'https://unification.com'
 
 GETH_PATH = '/bin/geth'
+DEFAULT_WRKCHAIN_DATA_DIR = '.und_wrkchain'
 
 WALLET_PASSWORD = 'password'
 PASSWORD_FILE = 'passwordfile'
