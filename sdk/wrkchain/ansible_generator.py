929e0853c92e2173b42558bd0eaf22bae54019ed
Indika Piyasena <indika.piyasena@gmail.com>
2019-04-09 12:44:18 +0800

    ansible.services: services is now a standard role with host_vars

--- sdk/wrkchain/ansible_generator.py
+++ sdk/wrkchain/ansible_generator.py
@@ -160,13 +160,13 @@ def transform_to_node_map(wrkchain_cfg):
     return ret
 
 
-def inject_geth_command(bootnode_config, wrkchain_id, workchain_nodes):
+def inject_command(bootnode_config, wrkchain_id, workchain_nodes):
     for key, validator in workchain_nodes.items():
         cmd = generate_geth_cmd(
             validator, bootnode_config, wrkchain_id,
             validator['docker_listen_port'],
             gopath='/home/deploy/.go', docker=False, path_to='/home/deploy')
-        validator['geth_command'] = cmd
+        validator['command'] = cmd
     return workchain_nodes
 
 
@@ -178,8 +178,7 @@ def process_custom_roles(template_root, ansible_dir, node_map):
 
         for node, node_d in node_map.items():
             d = {
-                'files/motd': node_d,
-                'templates/geth.service': node_d,
+                'files/motd': node_d
             }
             apply_custom_role(source, ansible_dir, role_name, node, d)
 
@@ -212,7 +211,7 @@ def write_keys(build_root: Path, name: str):
     target_public.chmod(0o600)
 
 
-def build_host_vars(build_root: Path, t_root: Path, validators: Validators):
+def build_host_vars(build_root: Path, t_root: Path, workchain_nodes):
     host_var_dir = build_root / 'ansible' / 'host_vars'
     if not host_var_dir.exists():
         host_var_dir.mkdir(parents=True)
@@ -221,7 +220,7 @@ def build_host_vars(build_root: Path, t_root: Path, validators: Validators):
     environment = Environment(loader=loader, undefined=DebugUndefined)
     template = environment.get_template('host_vars/host_var_file.yml')
 
-    for context in validators.context:
+    for key, context in workchain_nodes.items():
         dest = host_var_dir / f"{context['name']}.yml"
         dest.write_text(template.render(context))
 
@@ -235,14 +234,19 @@ def generate_ansible(build_dir, config, configured_bootnode_cfg):
     wrkchain_cfg = config['wrkchain']
     bootnode = Bootnode(wrkchain_cfg['bootnode'])
     validator_builder = Validators(
-        build_root, wrkchain_cfg['nodes'], ['bash', 'services'])
+        build_root, wrkchain_cfg['nodes'], ['bash'])
 
     # copy the password file before all else
     password_file = template_root() / 'ansible' / PASSWORD_FILE
     if not ansible_dir.exists():
         ansible_dir.mkdir(parents=True)
 
-    build_host_vars(build_root, templates, validator_builder)
+    network_id = config['wrkchain']['wrkchain_network_id']
+    node_map = transform_to_node_map(wrkchain_cfg)
+    workchain_nodes = inject_command(
+        configured_bootnode_cfg, network_id, node_map)
+
+    build_host_vars(build_root, templates, workchain_nodes)
 
     # Generate some keys pairs
     write_keys(build_root, 'id_rsa_root')
@@ -266,10 +270,6 @@ def generate_ansible(build_dir, config, configured_bootnode_cfg):
     }
     template_map(templates / 'ansible', ansible_dir, d)
 
-    network_id = config['wrkchain']['wrkchain_network_id']
-    node_map = transform_to_node_map(wrkchain_cfg)
-    workchain_nodes = inject_geth_command(
-        configured_bootnode_cfg, network_id, node_map)
     process_custom_roles(templates, ansible_dir, workchain_nodes)
 
     # Post Processing
